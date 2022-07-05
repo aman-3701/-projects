588e3df4e9adf3c55098121de7ea04349cc8a865
Aman Kumar Yadav <102834500+aman-3701@users.noreply.github.com>
2022-07-06 02:06:26 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+// int main (){
+//     int i , size ='5', arr[size];
+//     for (i = 0; i <size ; i++)
+//     scanf ("%d ", &arr[i]);
+//     for (i =   0; i < size ; i++)
+//     printf ("%d", arr[i]) ;
+//     return 0 ;
+
+// }
+// void func( int arr[]);
+// void swapvar(int a , int b);
+// void swaparr (int arr[5], int arr2[5]);
+int main (void){
+//     int i = 0, j = 0 , arr[6]={4,2,6 , 0 , 5,10};
+//     while (arr[i]){
+//         j += arr[i];
+//         i++;
+
+//     }
+// printf("j = %d \n", j);
+// return 0;
+//     int arr[5] = {5, 10 , 15, 20 , 25};
+//     func (arr);
+//     return 0;
+// }
+// void func (int arr[])
+// {
+//     int  i = 5 , sum = 0;
+//     while (i>2)
+//      sum = sum + arr[--i];
+//        printf("sum = %d\n", sum); /* code */
+// int a = 4 , b = 6;
+// int arr1[5]  = {1,2,3,4,5};
+// int arr2[5] = {6,7,8,9,10};
+// swapvar(a,b);
+// swaparr(arr1 , arr2);
+// printf("a= %d,b = %d\n", a ,b);
+// printf("arr1[0]=  %d, arr1[4]  = %d\n",arr1[0], arr1[4] );
+// printf("arr2 [0] = %d, arr2[4] = %d\n",arr2[0],arr2[4]);
+// return 0 ;}
+// void swapvar (int a , int b){
+//     int temp ;
+//     temp = a, a=b, b= temp;
+// }
+// void swaparr(int arr1[5],int arr2[5])
+// {
+//     int i , temp;
+//     for(i = 0; i<5; i++)
+//     {temp =arr1[i],arr1[i] = arr2[i],arr2[i] = temp;}
+}
